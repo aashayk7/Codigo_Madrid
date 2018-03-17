ab95e109fd762fc1598269bf917165d4abe6e19b
ManasMahajan <34194973+ManasMahajan@users.noreply.github.com>
2018-03-17 16:58:34 +0530

    c Program t print first n rows of floyd's triangle

---
+++ floyds_triangle.c
@@ -0,0 +1,25 @@
+//Floyd's triangle
+#include<stdio.h>
+
+int floyds_triangle(int n)
+{
+	int num=1,line_num=1;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=line_num;j++)
+		{
+			printf("%d ",num);
+			++num;
+		}
+		line_num++;
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int n;
+	scanf(" %d",&n);
+	floyds_triangle(n);
+	return 0;
+}
